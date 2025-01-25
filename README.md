420872e221529ff78703fb336416a8af42fb9112
Ryan Govostes <rgovostes@whoi.edu>
2025-01-24 16:15:19 -0800

    Split login into create-client/get-token

--- README.md
+++ README.md
@@ -11,11 +11,17 @@ The library also provides a command line utility:
 
     $ peplinkctl --help
     usage: peplinkctl [-h] -s SERVER [-c CLIENT] [-k] [-t TOKEN]
-                      {login,get-status-wan-connection}
+                      {create-client,get-token,get-status-wan-connection}
 
     $ PEPLINK_USER=admin \
       PEPLINK_PASSWORD=s3cr3t \
-      peplinkctl -k -s https://192.168.1.1 -c CLI login
+      peplinkctl -k -s https://192.168.1.1 create-client -c CLI
+    CLIENT_ID="026a74c97807e2dc3385c99ce90f80f8"
+    CLIENT_SECRET="75c61f25908c67a4d157cad9c6f7ca55"
+
+    $ CLIENT_ID="026a74c97807e2dc3385c99ce90f80f8" \
+      CLIENT_SECRET="75c61f25908c67a4d157cad9c6f7ca55" \
+      peplinkctl -k -s https://192.168.1.1 get-token
     TOKEN="e527e5468d85a577e6620c0b5f1f253c"
     EXPIRATION="2025-01-26 23:24:06.471771+00:00"
 
--- src/peplinkctl.py
+++ src/peplinkctl.py
@@ -25,13 +25,13 @@ def set_ssl_verify(verify=True):
     ctx.verify_mode = ssl.CERT_NONE if not verify else ssl.CERT_REQUIRED
 
 
-def assert_ok(response):
+def assert_ok(response: dict):
     if not response.get('stat') == 'ok':
         raise Exception(f'API error: {response.get("code")} - '
                         f'{response.get("message")}')
 
 
-def get_credentials():
+def get_login_credentials():
     username = os.environ.get('PEPLINK_USER')
     password = os.environ.get('PEPLINK_PASSWORD')
     if not username:
@@ -41,59 +41,73 @@ def get_credentials():
     return username, password
 
 
-def login(server, username, password, client):
-    # Log in with username and password; gives us a session cookie
+def get_client_credentials():
+    client_id = os.environ.get('CLIENT_ID')
+    client_secret = os.environ.get('CLIENT_SECRET')
+    if not client_id or not client_secret:
+        raise Exception('CLIENT_ID and CLIENT_SECRET environment variables '
+                        'must be set')
+    return client_id, client_secret
+
+
+def login(server: str, username: str, password: str):
+    '''Log in with username and password'''
+
     req = urllib.request.Request(
         f'{server}/api/login',
-        headers={
-            'Content-Type': 'application/json'
-        },
+        headers={'Content-Type': 'application/json'},
         data=json.dumps({
             'username': username,
-            'password': password
+            'password': password,
         }).encode(),
     )
     response = json.loads(urllib.request.urlopen(req).read())
     assert_ok(response)
 
-    # Query the set of configured API clients
-    response = json.loads(urllib.request.urlopen(f'{server}/api/auth.client').read())
+
+def get_or_create_client(server: str, name: str, readwrite: bool):
+    '''Get existing client credentials or create new ones'''
+
+    # Query existing clients
+    response = json.loads(
+        urllib.request.urlopen(f'{server}/api/auth.client').read()
+    )
     assert_ok(response)
 
-    # Look for an existing client with credentials
-    client_id, client_secret = None, None
+    # Look for existing client
     for c in response['response']:
-        if c['name'] == client:
-            client_id, client_secret = c['clientId'], c['clientSecret']
-            break
+        if c['name'] == name:
+            if (not readwrite) ^ (c['scope'] == 'api.read-only'):
+                # TODO: Client exists with different scope
+                raise NotImplementedError('Cannot recreate clients yet')
+            return c['clientId'], c['clientSecret']
 
-    # If there is no existing client, create one
-    if client_id is None:
-        req = urllib.request.Request(
-            f'{server}/api/auth.client',
-            method='POST',
-            headers={
-                'Content-Type': 'application/json'
-            },
-            data=json.dumps({
-                'action': 'add',
-                'name': client,
-                'scope': 'api.read-only'
-            }).encode(),
-        )
-        response = json.loads(urllib.request.urlopen(req).read())
-        assert_ok(response)
+    # Create new client
+    req = urllib.request.Request(
+        f'{server}/api/auth.client',
+        method='POST',
+        headers={'Content-Type': 'application/json'},
+        data=json.dumps({
+            'action': 'add',
+            'name': name,
+            'scope': 'api' if readwrite else 'api.read-only'
+        }).encode(),
+    )
+    response = json.loads(urllib.request.urlopen(req).read())
+    assert_ok(response)
 
-        client_id = response['response']['clientId']
-        client_secret = response['response']['clientSecret']
+    return (
+        response['response']['clientId'],
+        response['response']['clientSecret']
+    )
 
-    # Use the client credentials to request an API token
+
+def get_token(server: str, client_id: str, client_secret: str):
+    '''Get API token using client credentials'''
     req = urllib.request.Request(
         f'{server}/api/auth.token.grant',
         method='POST',
-        headers={
-            'Content-Type': 'application/json'
-        },
+        headers={'Content-Type': 'application/json'},
         data=json.dumps({
             'clientId': client_id,
             'clientSecret': client_secret,
@@ -120,12 +134,19 @@ def get_status_wan_connection(server, token):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--server', required=True)
-    parser.add_argument('-c', '--client')
     parser.add_argument('-k', '--no-verify-ssl', action='store_true')
     parser.add_argument('-t', '--token')
     subparsers = parser.add_subparsers(dest='command', required=True)
-    subparsers.add_parser('login')
-    subparsers.add_parser('get-status-wan-connection')
+
+    subparser = subparsers.add_parser('create-client')
+    subparser.add_argument('-c', '--client', required=True)
+    subparser.add_argument('--read-write', dest='rw', action='store_true')
+    subparser.add_argument('--read-only', dest='rw', action='store_false')
+
+    subparsers.add_parser('get-token')
+
+    subparser = subparsers.add_parser('get-status-wan-connection')
+
     args = parser.parse_args()
 
     args.server = args.server.rstrip('/')
@@ -133,18 +154,24 @@ def main():
     if args.no_verify_ssl:
         set_ssl_verify(False)
 
-    if args.command == 'login' and args.token:
-        parser.error('Cannot use token with login command')
+    if args.command == 'create-client':
+        username, password = get_login_credentials()
+        login(args.server, username, password)
+        client_id, client_secret = get_or_create_client(
+            args.server, args.client, args.rw)
+        print(f'CLIENT_ID="{client_id}"')
+        print(f'CLIENT_SECRET="{client_secret}"')
+        return
 
+    # If a token isn't provided, create one
     if not args.token:
-        if args.client is None:
-            parser.error('Missing API client name (-c/--client)')
-        username, password = get_credentials()
-        args.token, expiry = login(args.server, username, password, args.client)
+        client_id, client_secret = get_client_credentials()
+        args.token, expiry = get_token(args.server, client_id, client_secret)
 
-    if args.command == 'login':
+    if args.command == 'get-token':
         print(f'TOKEN="{args.token}"')
         print(f'EXPIRATION="{expiry}"')
+        return
 
     if args.command == 'get-status-wan-connection':
         status = get_status_wan_connection(args.server, args.token)
